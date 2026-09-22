c64278f535921ce05278c667d8598e64df61e33b
marcsegal <marcasegal@icloud.com>
2026-09-22 22:33:18 +0100

    MVP

--- Server/app.py
+++ Server/app.py
@@ -16,9 +16,9 @@ load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY", "tunegame-secret")
-app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 app.config['SESSION_COOKIE_SECURE'] = False
-CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+CORS(app, supports_credentials=True, origins=["http://localhost:63342", "http://localhost:8000"])
 
 SCORES_FILE = "scores.json"
 TIME_LIMIT = 120        # seconds to answer before time's up
